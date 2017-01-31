fc63c5c0da0568fa106614380fadde27ae989883
Kryštof Tulinger <k.tulinger@seznam.cz>
2017-01-31 22:15:08 +0100

    init

---
+++ src/PhotosExtension.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tulinkry\DI;
+
+class PhotosExtensions extends CompilerExtension
+{
+
+    private $defaults = array (
+    );
+
+    public function loadConfiguration () {
+    }
+
+    public function beforeCompile () {
+    }
+}
